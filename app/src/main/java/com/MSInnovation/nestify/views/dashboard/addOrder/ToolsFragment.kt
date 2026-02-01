36a59ea742ea3e0a70c8f6dbd22cbb361c33c188
Iqbal Hossain Sourov <princesourov41@gmail.com>
2026-02-01 14:24:53 +0600

    add order desig

---
+++ app/src/main/java/com/MSInnovation/nestify/views/dashboard/addOrder/ToolsFragment.kt
@@ -0,0 +1,15 @@
+package com.MSInnovation.nestify.views.dashboard.addOrder
+
+import com.MSInnovation.nestify.base.BaseFragment
+import com.MSInnovation.nestify.databinding.FragmentToolsBinding
+
+class ToolsFragment : BaseFragment<FragmentToolsBinding>(FragmentToolsBinding::inflate) {
+    override fun setListener() {
+
+    }
+
+    override fun allObserver() {
+
+    }
+
+}
